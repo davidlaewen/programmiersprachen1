d0aebf8527a24bfdb617841ec5a538279fc9007a
David Läwen <david.laewen@gmx.de>
2020-10-08 01:33:27 +0200

    VL12, n-fold continuation

---
+++ code/12b-ContinuationsAddNTimes.scala
@@ -0,0 +1,51 @@
+import scala.collection.mutable
+import scala.io.StdIn.readLine
+
+def inputNumber(prompt: String) : Int = {
+  println(prompt)
+  Integer.parseInt(readLine())
+}
+
+/** Normal style */
+def addAllCosts(items: List[String]): Int = items match {
+  case List() => 0
+  case first :: rest => inputNumber("Cost of "+first+":") + addAllCosts(rest)
+}
+
+val testList = List("Banana", "Apple", "Orange")
+def test : Unit = println("Total cost: " + addAllCosts(testList))
+
+
+/** Continuation style */
+def webDisplay(s: String) : Nothing = {
+  println(s)
+  sys.error("Program terminated")
+}
+
+val continuations = new mutable.HashMap[String, Int=>Nothing]
+var nextIndex : Int = 0
+def getNextId : String = {
+  nextIndex += 1
+  "c"+nextIndex
+}
+
+def continue(kId: String, result: Int): Nothing = continuations(kId)(result)
+
+def webReadCont(prompt: String, k: Int => Nothing) : Nothing = {
+  val id = getNextId
+  continuations += (id -> k)
+  println(prompt)
+  println("To continue, invoke continuation "+id)
+  sys.error("Program terminated")
+}
+
+def addAllCostsCont(itemList: List[String], k: Int => Nothing) : Nothing = {
+  itemList match {
+    case List() => k(0)
+    case first :: rest =>
+      webReadCont("Cost of "+first+":",
+        (n: Int) => addAllCostsCont(rest, (m: Int) => k(m+n)))
+  }
+}
+
+def testWeb : Unit = addAllCostsCont(testList, m => webDisplay("Total cost: " + m))
